3ee26a9fe701d8233a831a21274083388484118b
BRIAN YACK <bdyack@gmail.com>
2017-09-12 10:15:50 -0500

    change

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
 	var searchQuery;
 	var currentPlay;
-	var topics= ["Tiger Shark", "Great White Shark", "Megamouth Shark", "Goblin Shark", "Carribean Reef Shark", "Sand Tiger Shark", "Sawfish", "Thresher Shark", "Blue Shark", "Sevengill Shark"];
+	var sharks= ["Tiger Shark", "Great White Shark", "Megamouth Shark", "Goblin Shark", "Carribean Reef Shark", "Sand Tiger Shark", "Sawfish", "Thresher Shark", "Blue Shark", "Sevengill Shark"];
 	
