9df033154c910394babc3f4fd149696e70bcdd90
BRIAN YACK <bdyack@gmail.com>
2017-09-12 10:13:51 -0500

    starting out jscript

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	var searchQuery;
+	var currentPlay;
+	var topics= ["Tiger Shark", "Great White Shark", "Megamouth Shark", "Goblin Shark", "Carribean Reef Shark", "Sand Tiger Shark", "Sawfish", "Thresher Shark", "Blue Shark", "Sevengill Shark"];
+	
