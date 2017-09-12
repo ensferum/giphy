254e75337cf51b0a536dd953ada17910f0b76d41
BRIAN YACK <bdyack@gmail.com>
2017-09-12 10:17:49 -0500

    syntax error fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,3 +3,35 @@ $(document).ready(function() {
 	var currentPlay;
 	var sharks= ["Tiger Shark", "Great White Shark", "Megamouth Shark", "Goblin Shark", "Carribean Reef Shark", "Sand Tiger Shark", "Sawfish", "Thresher Shark", "Blue Shark", "Sevengill Shark"];
 	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
