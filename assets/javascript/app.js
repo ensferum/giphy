ab0367c48e9999b137c2a5a75ad8e8a090d827ea
BRIAN YACK <bdyack@gmail.com>
2017-09-13 11:37:08 -0500

    couple changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,90 +1,86 @@
 $(document).ready(function() {
-	var searchQuery;
-	var currentPlay;
-	var sharks= ["Tiger Shark", "Great White Shark", "Megamouth Shark", "Goblin Shark", "Leopard Shark", "Sand Tiger Shark", "Basking Shark", "Thresher Shark", "Whale Shark", "Sevengill Shark"];
-	
+    var searchQuery;
+    var currentPlay;
+    var topics = ["Tiger Shark", "Great White Shark", "Megamouth Shark", "Goblin Shark", "Leopard Shark", "Sand Tiger Shark", "Basking Shark", "Thresher Shark", "Whale Shark", "Sevengill Shark"];
 
 
+    renderButtons();
 
-
-
-renderButtons();
     function renderButtons() {
         $("#buttonRow").empty();
-        for (var i = 0; i < sharks.length; i++) {
-            var button=$("<button>");
-            button.addClass("Generate-gallery");
-            button.data("Sharks", sharks[i]);
+        for (var i = 0; i < topics.length; i++) {
+            var button = $("<button>");
+            button.addClass("makeGallery");
+            button.data("topics", topics[i]);
             button.addClass("btn spacing");
-            button.text(sharks[i]);
+            button.text(topics[i]);
             $("#buttonRow").append(button);
         }
     }
     $("#add-artist").on("click", function() {
         event.preventDefault();
-        var input=$("#submit-input").val();
-        if (topics.indexOf(input)===-1&&input!="")
-        {
-          sharks.push(input);
-          sharks.sort();
-          renderButtons();
+        var input = $("#submit-input").val();
+        if (topics.indexOf(input) === -1 && input != "") {
+            topics.push(input);
+            topics.sort();
+            renderButtons();
         }
     });
-    $(document).on("click", ".Generate-gallery", function() {
-        if(searchQuery===$(this).data("Sharks"))
+
+
+
+    $(document).on("click", ".makeGallery", function() {
+        if (searchQuery === $(this).data("topics"))
             return;
-        searchQuery=$(this).data("Sharks");
-        currentPlay=undefined;
+        searchQuery = $(this).data("topics");
+        currentPlay = undefined;
         $("#gallery").empty();
-        $("#gallery").append("<h1 class='col-md-12'>"+searchQuery+"<h1>");
-        while (searchQuery.indexOf(" ")!=-1)
-        {
-            searchQuery=searchQuery.replace(" ","+");
-        }       
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q="+encodeURI(searchQuery) + "&limit=10&api_key=dc6zaTOxFJmzC";
+        $("#gallery").append("<h1 class='col-md-12'>" + searchQuery + "<h1>");
+        while (searchQuery.indexOf(" ") != -1) {
+            searchQuery = searchQuery.replace(" ", "+");
+        }
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + encodeURI(searchQuery) + "&limit=10&api_key=dc6zaTOxFJmzC";
         $.ajax({
             url: queryURL,
-             method: 'GET'
+            method: 'GET'
         }).done(function(response) {
             for (var i = 0; i < response.data.length; i++) {
-                var entry=$("<div>");
-                var img= $("<img>");
+                var entry = $("<div>");
+                var img = $("<img>");
                 $(entry).addClass("Canvas col-md-3");
-                $(entry).append("<h2> rating: "+ response.data[i].rating+ "<h2>");
+                $(entry).append("<h2> Rating: " + response.data[i].rating + "<h2>");
                 $(img).attr("src", response.data[i].images.fixed_width_still.url);
                 $(img).data("giphyData", response.data[i]);
                 $(img).addClass("Play");
                 $(entry).append(img);
                 $("#gallery").append(entry);
-            }   
+            }
         });
     });
+
+
+
     $(document).on("click", ".Play", function() {
-        if(currentPlay!=undefined)
-        {
+        if (currentPlay != undefined) {
             $(currentPlay).attr("src", $(currentPlay).data("giphyData").images.fixed_width_still.url);
         }
-        if(currentPlay===this)
-        {
-            currentPlay=undefined;
+        if (currentPlay === this) {
+            currentPlay = undefined;
             return;
         }
-        currentPlay=this;
+        currentPlay = this;
         $(this).attr("src", $(this).data("giphyData").images.fixed_width.url);
     });
     $(document).on("mouseenter", ".Play", function() {
-        if(currentPlay===this)
-        {
+        if (currentPlay === this) {
             return;
         }
         $(this).attr("src", $(this).data("giphyData").images.fixed_width.url);
     });
     $(document).on("mouseleave", ".Play", function() {
-        if(currentPlay===this)
-        {
+        if (currentPlay === this) {
             return;
         }
         $(this).attr("src", $(this).data("giphyData").images.fixed_width_still.url);
     });
 });
-
